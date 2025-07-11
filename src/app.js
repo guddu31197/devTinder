d46814e16b897a7106d63946341fb6761c53b200
guddu31197gmail.com <guddu31197>
2025-07-11 12:36:23 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,24 +2,32 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Randhir", lastName: "Kumar" });
-});
-
-app.post("/user", (req, res) => {
-  // saving data to DB
-  res.send("Data successfully saved to the database!");
-});
-
-app.delete("/user", (req, res)=>{
-  res.send("Delete successfully!")
-})
-
-// This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
+  ],
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    res.send("5th Response!!");
+  },
+);
 
 app.listen(7777, () => {
   console.log("server is succesfully liistening on port 7777...");
