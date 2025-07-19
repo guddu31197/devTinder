0bc7411a3a575ec0635c33aa652bc42ee4ef8169
guddu31197gmail.com <guddu31197>
2025-07-19 16:53:34 +0530

    logout editprofile api created

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,6 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth.js");
 
-
 profileRouter.get("/profile", userAuth, async (req, res) => {
   try {
     const user = req.user;
